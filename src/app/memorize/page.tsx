d94ee90be1df8469313b2621972f0c5fe180c8e9
Asano <asanowork0913@gmail.com>
2025-09-07 22:01:39 +0900

    ハンバーガーメニュー追加

--- src/app/memorize/page.tsx
+++ src/app/memorize/page.tsx
@@ -1,19 +1,13 @@
 import WeakCardsComponent from "@/components/WeakCardsComponent";
-import {
-  // FetchCard,
-  FetchCardSupa,
-  // FetchWeakCard,
-  // FetchWeakCardSupa,
-} from "@/lib/FetchCard";
-import { getSession } from "@/lib/session"; // 作成した関数をインポート
+// import { getSession } from "@/lib/session"; // 作成した関数をインポート
 import { Suspense } from "react";
 
-const MemorizePage = async () => {
-  const user = await getSession();
-  const userId = user?.id;
+const MemorizePage = () => {
+  // const user = await getSession();
+  // const userId = user?.id;
 
   // 必須で高速なデータだけを先に取得する
-  const allCards = await FetchCardSupa({});
+  // const allCards = await FetchCardSupa({});
   // console.log(allCards)
   // ローディング中に表示するシンプルなコンポーネント
   const LoadingSpinner = () => {
@@ -27,7 +21,7 @@ const MemorizePage = async () => {
   return (
     <div>
       <Suspense fallback={<LoadingSpinner />}>
-        <WeakCardsComponent userId={userId} allCards={allCards} />
+        <WeakCardsComponent />
       </Suspense>
     </div>
   );
--- src/components/WeakCardsComponent.tsx
+++ src/components/WeakCardsComponent.tsx
@@ -1,16 +1,19 @@
 // app/memorize/WeakCardsComponent.js
 
-import { FetchWeakCardSupa } from "@/lib/FetchCard";
+import { FetchCardSupa, FetchWeakCardSupa } from "@/lib/FetchCard";
 import { Card } from "@/type/types";
 import Memorizepage from "./MemorizePage";
+import { getSession } from "@/lib/session";
 interface WeakCardsComponentProps {
   userId: string | undefined;
   allCards: Card[];
 }
-const WeakCardsComponent = async ({
-  userId,
-  allCards,
-}: WeakCardsComponentProps) => {
+const WeakCardsComponent = async () => {
+  const user = await getSession();
+  const userId = user?.id;
+
+  // 必須で高速なデータだけを先に取得する
+  const allCards = await FetchCardSupa({});
   // ユーザーが存在する場合のみ、時間のかかる苦手カードのデータを取得
   const weakCards = userId ? await FetchWeakCardSupa(userId) : [];
 
