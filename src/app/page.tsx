19c01038aa1c4ef8650b394fdd7f7b94612597e8
Asano <asanowork0913@gmail.com>
2025-09-07 20:37:49 +0900

    トップ画面デザイン変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,11 @@ export default function Home() {
     <div className="bg-green-100">
       <div className="container mx-auto min-h-screen flex justify-center items-center">
         <Header />
-        <div className="flex flex-col items-center md:flex-row justify-evenly w-full text-white mt-5 sm:gap-7 md:gap-20 md-mt-0">
+        <div className="flex flex-col items-center gap-10 md:flex-row justify-evenly w-full text-white mt-5 sm:gap-7 md:gap-20 md-mt-0">
           {link_data.map((item) => (
             <Link
               href={item.path}
-              className="bg-green-400 rounded-full w-40 h-40 md:w-80 md:h-80 p-20 text-4xl cursor-pointer flex items-center justify-center transform transition hover:scale-105 hover:bg-green-500 shadow-md"
+              className="bg-green-400 rounded-full text-2xl w-60 h-60 md:w-80 md:h-80 p-20 md:text-4xl cursor-pointer flex items-center justify-center transform transition hover:scale-105 hover:bg-green-500 shadow-md"
               key={item.name}
             >
               <p>{item.name}</p>
