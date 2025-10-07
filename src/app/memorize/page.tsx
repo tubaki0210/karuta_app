3bb2d78e476923df46d8796933d0776845bcee56
Asano <asanowork0913@gmail.com>
2025-10-07 21:27:41 +0900

    Readme変更

--- src/app/memorize/page.tsx
+++ src/app/memorize/page.tsx
@@ -11,13 +11,15 @@ const MemorizePage = async () => {
   };
 
   return (
-    <div className="py-25">
-      <h1 className="text-center text-3xl border-2 py-2 bg-white rounded-md">
-        百人一首一覧
-      </h1>
-      <Suspense fallback={<LoadingSpinner />}>
-        <WeakCardsComponent />
-      </Suspense>
+    <div>
+      <div className="bg-green-100 py-25">
+        <h1 className="text-center text-3xl border-2 py-2 bg-white rounded-md">
+          百人一首一覧
+        </h1>
+        <Suspense fallback={<LoadingSpinner />}>
+          <WeakCardsComponent />
+        </Suspense>
+      </div>
     </div>
   );
 };
