ac124c19cf7125756ceb4b385146bda423ed1310
Asano <asanowork0913@gmail.com>
2025-10-07 21:28:22 +0900

    Readme変更

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -2,10 +2,8 @@ import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export async function getSession() {
-  // 1. まず `cookies()` を呼び出して、クッキーのインスタンス（ストア）を取得します。
   const cookieStore = cookies();
 
-  // 2. Supabaseクライアントには、そのインスタンスを返す「関数」を渡します。
   // createServerComponentClientはサーバコンポーネント内で読み込みのみ、ユーザ情報を取得する場合のみ
   const supabase = createServerComponentClient({
     cookies: () => cookieStore,
