9017d210eb84c170964467deffd18ea65038bc56
Asano <asanowork0913@gmail.com>
2025-12-09 22:25:18 +0900

    不要なSuspenceを削除

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 import LoginForm from "@/components/LoginForm";
+import { Suspense } from "react";
 
 const LoginPage = () => {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
       <div className="flex justify-center items-center min-h-screen">
         <LoginForm />
       </div>
+      </Suspense>
   );
 };
 
